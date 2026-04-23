af2e0b149aedce1ae1a8720390b7b5177ad5775b
vardanyann17 <vardanyankar3@gmail.com>
2026-04-23 20:08:37 +0400

    uxxaki

---
+++ first_step.py
@@ -0,0 +1,12 @@
+# def palindrome (x):
+#     x=str(x)
+#     if x==x[::-1]:
+#         return(True)
+#     else:
+#         return(False)
+# # print(palindrome(555)) 
+# def length(x):
+#     s=x.split()
+#     return len(s[-1])
+# print(length('hello word'))
+        
